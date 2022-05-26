5c75588b432d7d5101afb17f08f53e9b4ba257f3
brennop <brennopcordeiro@gmail.com>
2022-05-26 12:22:11 -0300

    ✨ improve post component

---
+++ components/post.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mt-2 px-2 py-4 border-b border-gray-100 text-gray-900">
+    <p>{{ post.poster.username }}
+
+    <div class="select-none text-sm animate-bounce inline-block" v-if="isOnFire"
+      :title="post.poster.username + ' is on a ' + post.poster.streak + ' day streak'">🔥
+    </div>
+    <span class="text-sm"> at {{ time }}</span>
+    </p>
+    <p class="mt-2">{{ post.text }}</p>
+  </div>
+</template>
+
+<script setup>
+const { post } = defineProps(["post"]);
+
+const isOnFire = post.poster.streak > 0;
+const time = new Date(post.created_at).toLocaleTimeString()
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,14 @@
 <template>
   <template v-if="!posted">
+    <marquee>Check-in first to see what others are up to!</marquee>
     <form class="mt-2" @submit.prevent="submitPost">
       <textarea class="border border-gray-900 w-full p-1" v-model="text" />
       <input type="submit" class="w-full border border-gray-900 px-4 py-2 mt-2"
-        :value="loading ? 'Loading ...' : 'post'" :disabled="loading" />
+        :value="loading ? 'Loading ...' : 'check-in'" :disabled="loading" />
     </form>
   </template>
 
-  <div v-for="post in posts" class="mt-2 px-2 py-4 border-b border-gray-100">
-    <p>{{ post.poster.username }} at {{ new
-        Date(post.created_at).toLocaleTimeString()
-    }}</p>
-    <p :key="post.id" class="mt-2">{{ post.text }}</p>
-  </div>
+  <post v-for="post in posts" :post="post" :key="post.id" />
 </template>
 
 <script setup>
@@ -32,6 +28,7 @@ async function submitPost() {
       .from("posts")
       .insert([{ text: text.value }], { returning: "minimal" });
 
+    window.location.reload();
   } catch (error) {
     alert(error.message)
   } finally {
